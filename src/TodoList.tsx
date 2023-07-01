ecf6014b898e3b4bee972e2145139c76ae0745db
Mitch Ryan <mitch@mitch-ryan.com>
2023-07-01 13:23:47 +0200

    Implement roving tabindex on todolist

--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -1,17 +1,23 @@
 import { useAppState } from "./AppStateContext";
 import styled from "styled-components";
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 
 const Container = styled.div`
   display: flex;
   flex-direction: column;
+  border-radius: 2px;
+  border: 2px solid transparent;
+
+  &:has(:focus) {
+    border: 2px solid white;
+  }
 `;
 
 const TodoWrapper = styled.div`
   display: flex;
 `;
 
-const TodoButton = styled.button`
+const TodoButton = styled.button<{ $active?: boolean }>`
   background: none;
   border: none;
   appearance: none;
@@ -51,6 +57,8 @@ export const TodoList = () => {
     }));
   };
 
+  const [activeIndex, setActiveIndex] = useState<number>(0);
+
   const wrapperRef = useRef<HTMLDivElement | null>(null);
 
   const onKeyDown = (event: React.KeyboardEvent) => {
@@ -66,23 +74,34 @@ export const TodoList = () => {
       case "ArrowUp":
       case "ArrowLeft": {
         event.preventDefault(); // Block scrolling
-        allButtons.at(currentIndex - 1)?.focus();
+        const newIndex = Math.max(
+          0,
+          (currentIndex - 1 + allButtons.length) % allButtons.length
+        );
+        setActiveIndex(newIndex);
+        allButtons.at(newIndex)?.focus();
         break;
       }
       case "ArrowDown":
       case "ArrowRight": {
         event.preventDefault(); // Block scrolling
-        allButtons.at((currentIndex + 1) % allButtons.length)?.focus();
+        const newIndex = (currentIndex + 1) % allButtons.length;
+        setActiveIndex(newIndex);
+        allButtons.at(newIndex)?.focus();
         break;
       }
       case "Home": {
         event.preventDefault(); // Block scrolling
-        allButtons.at(0)?.focus();
+        const newIndex = 0;
+        setActiveIndex(newIndex);
+        allButtons.at(newIndex)?.focus();
         break;
       }
       case "End": {
         event.preventDefault(); // Block scrolling
-        allButtons.at(-1)?.focus();
+        const newIndex = Math.max(0, allButtons.length - 1);
+        setActiveIndex(newIndex);
+        allButtons.at(newIndex)?.focus();
         break;
       }
     }
@@ -90,12 +109,17 @@ export const TodoList = () => {
 
   return (
     <Container ref={wrapperRef}>
-      {appState.todos.map((todo) => (
+      {appState.todos.map((todo, index) => (
         <TodoWrapper key={todo.uuid}>
           <TodoButton
-            onClick={() => toggleTodoCompleted(todo.uuid)}
-            data-todolist-button
             onKeyDown={onKeyDown}
+            tabIndex={activeIndex === index ? 0 : -1} // using "roving" tabIndex
+            onClick={() => {
+              setActiveIndex(index);
+              toggleTodoCompleted(todo.uuid);
+            }}
+            data-todolist-button
+            $active={activeIndex === index}
           >
             <TodoTitle $completed={todo.completed}>{todo.title}</TodoTitle>
           </TodoButton>
