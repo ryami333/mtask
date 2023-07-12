d38f2644c292707c8eebec39b3e7a5cd38be22de
Mitch Ryan <mitch@mitch-ryan.com>
2023-07-12 13:13:13 +0200

    Log config params

--- src/helpers/config.ts
+++ src/helpers/config.ts
@@ -4,3 +4,5 @@ import { resolve } from "node:path";
 export const APP_DATA_DIRECTORY = resolve(app.getPath("appData"), "mtask");
 
 export const DB_PATH = resolve(APP_DATA_DIRECTORY, "db.json");
+
+console.dir({ APP_DATA_DIRECTORY, DB_PATH });
