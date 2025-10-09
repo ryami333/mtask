14bfd9bfc1773e3eed3efaf49f1eae92e2fff4d1
Mitch Ryan <mitch@diesdas.digital>
2025-10-09 12:12:15 +0200

    Bump to latest Storybook, use vite loader instead of Webpack

--- src/components/Button.stories.tsx
+++ src/components/Button.stories.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Meta, StoryFn } from "@storybook/react";
+import { Meta, StoryFn } from "@storybook/react-vite";
 import { Button } from "./Button";
 
 type ButtonProps = React.ComponentProps<typeof Button>;
