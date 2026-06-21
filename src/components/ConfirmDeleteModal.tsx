df806a7a7771cf09cbe005ffae134b140ad8340e
Mitch Ryan <mitch@mitch-ryan.org>
2026-06-21 17:22:12 +0200

    Use FocusGroup in ConfirmDeleteModal

--- src/components/ConfirmDeleteModal.tsx
+++ src/components/ConfirmDeleteModal.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Modal } from "./Modal";
 import { Button } from "./Button";
+import { FocusGroup } from "./FocusGroup";
 import styles from "./ConfirmDeleteModal.module.css";
 import classNames from "classnames/bind";
 
@@ -19,10 +20,12 @@ export function ConfirmDeleteModal({
     <Modal isOpen={isOpen} onRequestClose={onRequestClose}>
       <div className={cx("inner")}>
         <p>Are you sure you wish to delete this item?</p>
-        <Button onClick={onRequestClose}>Cancel</Button>
-        <Button onClick={onConfirm} autoFocus>
-          Confirm
-        </Button>
+        <FocusGroup direction="inline" className={cx("buttons")}>
+          <Button onClick={onRequestClose}>Cancel</Button>
+          <Button onClick={onConfirm} autoFocus>
+            Confirm
+          </Button>
+        </FocusGroup>
       </div>
     </Modal>
   );
