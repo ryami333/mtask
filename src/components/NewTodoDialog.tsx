e071ad189997aee1f6c284483bd7c298af106d1e
Mitch Ryan <mitch@mitch-ryan.org>
2026-06-21 12:25:08 +0200

    Do first-pass new TODO dialog

---
+++ src/components/NewTodoDialog.tsx
@@ -0,0 +1,55 @@
+import {
+  useClickOutside,
+  useFocusTrap,
+  useInputState,
+  useMergedRef,
+} from "@mantine/hooks";
+import React, { Ref } from "react";
+import { Todo } from "../helpers/appState";
+import { Input } from "./Input";
+
+export function NewTodoDialog({
+  isOpen,
+  onRequestClose,
+  inputRef,
+  onSubmit,
+}: {
+  isOpen: boolean;
+  onRequestClose: () => void;
+  inputRef: Ref<HTMLInputElement>;
+  onSubmit: (todo: Todo) => void;
+}) {
+  const focusTrapRef = useFocusTrap(isOpen);
+  const clickawayref = useClickOutside(onRequestClose);
+  const [inputValue, setInputValue] = useInputState("");
+
+  const mergedRef = useMergedRef(focusTrapRef, clickawayref);
+  return (
+    <dialog open={isOpen} ref={mergedRef}>
+      <form
+        autoFocus
+        // className={cx("form")}
+        onSubmit={(e) => {
+          e.preventDefault();
+
+          if (inputValue) {
+            onSubmit({
+              uuid: crypto.randomUUID(),
+              title: inputValue,
+              completed: false,
+            });
+          }
+          setInputValue("");
+        }}
+      >
+        <Input
+          ref={inputRef}
+          type="text"
+          value={inputValue}
+          onChange={setInputValue}
+          placeholder="Please enter a new thing here"
+        />
+      </form>
+    </dialog>
+  );
+}
